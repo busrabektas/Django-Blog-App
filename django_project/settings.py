2cd1df14e8110ae97e9329e3656865d0bf222cae
busrabektas <busraasbektas@gmail.com>
2023-09-02 17:58:36 +0300

    .gitignore added

--- django_project/settings.py
+++ django_project/settings.py
@@ -131,4 +131,6 @@ LOGIN_URL = 'login'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
